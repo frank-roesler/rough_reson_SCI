61f353f08b9dd3c8132be6fbd5b0e26382c69c3f
Frank Rosler <frankrosler@FranksWrkLaptop.fritz.box>
2022-06-10 16:50:46 +0200

    update compute_M_in_precise.m. Now integrals are more precise

---
+++ compute_matrix_elements/compute_M_in_fem_precise.m
@@ -0,0 +1,41 @@
+function M_in = compute_M_in_fem_precise(k,N,c4n,n4e,Nb,fNodes,r_ball)
+% Computes matrix approximation for M_inner at specified value of k
+% directly from the FEM solution u.
+    [nC,d] = size(c4n);
+    M_in = zeros(2*N+1);
+    Alpha = -N:N;
+    x1 = c4n(Nb(:,1),:); % boundary of disk
+    x2 = c4n(Nb(:,2),:); % boundary of disk
+    t_1 = atan2(x1(:,2),x1(:,1));
+    t_2 = atan2(x2(:,2),x2(:,1));
+    dt = mod(t_2-t_1,2*pi);
+    
+    int_exp = (exp(1i*Alpha.*t_2) - exp(1i*Alpha.*t_1))./(1i*Alpha*sqrt(2*pi*r_ball));
+    int_exp(:,N+1) = mod(t_2 - t_1, 2*pi)/sqrt(2*pi*r_ball);
+    
+    int_t_exp = exp(1i*Alpha.*t_1)./(Alpha.^2*sqrt(2*pi*r_ball)).*(exp(1i*Alpha.*dt).*(1-1i*Alpha.*dt) - 1);
+    int_t_exp(:,N+1) = dt.^2./(2*sqrt(2*pi*r_ball));
+    
+    [s,m] = fe_matrices(c4n,n4e,Nb);
+    S = s - k^2*m;   % weak version of -∆-k^2
+    for alpha=-N:N
+        b = zeros(nC,1);
+        for j = 1:size(Nb,1)
+            vol_S = norm(c4n(Nb(j,1),:)-c4n(Nb(j,2),:));
+            mp_S  = sum(c4n(Nb(j,:),:),1)/d;
+            for i = 1:d
+                b(Nb(j,i)) = b(Nb(j,i))+(1/d)*vol_S*e_alpha(alpha,r_ball,mp_S);
+            end
+        end
+        u         = zeros(nC,1);
+        u(fNodes) = S(fNodes,fNodes)\b(fNodes); 
+        
+        u1 = u(Nb(:,1));
+        u2 = u(Nb(:,2));
+        dudt = (u2-u1)./dt;
+        int_A = int_exp'*u1;
+        int_B = int_t_exp'*dudt;
+        M_in(alpha+N+1,:) = r_ball*(int_A + int_B);
+    end
+    M_in = M_in.';
+end
--- compute_matrix_elements/main.m
+++ compute_matrix_elements/main.m
@@ -1,54 +1,52 @@
 clear
 addpath('/Users/frankrosler/Documents/MATLAB/matplotlib')
-mesh_path = 'meshes/julia0.02.mat';
-upper_bound_eigs = 100;
+mesh_path = 'meshes/helmholtz0.05.mat';
+upper_bound_eigs = 200;
 [W,spectrum,c4n,n4e,fNodes,Nb,r_ball] = compute_eigenfunctions(mesh_path,upper_bound_eigs);
 disp(['Eigenfunctions done. Found ',num2str(size(W,2)),' eigenfunctions.'])
 
 %% Precompute M_in(k_0):
 N = 30; % half size of matrix M_in^n(k)
 k_0 = -0.5i;
-M_in_direct_0 = compute_M_in_fem(k_0,N,c4n,n4e,Nb,fNodes,r_ball);
+M_in_direct_0 = compute_M_in_fem_precise(k_0,N,c4n,n4e,Nb,fNodes,r_ball);
 cN = diag(abs(-N:N)); cN(N+1,N+1)=1; cN = sqrt(cN);
 
 %% Compute det(M_in + M_out) in complex plane:
-z1 = -4-1i;
-z2 = 4;
-hres = 100;
+z1 = 1-0.1i;
+z2 = 3;
+hres = 500;
 L = build_lattice(z1, z2, hres);
 dets = zeros(size(L));
-
 
 parfor i=1:length(L(:))
     k = L(i);
     corrector = atkinson(k_0,k,c4n,Nb,N,W,spectrum,r_ball);
     M_in = M_in_direct_0 + corrector;
-%     M_in = compute_M_in_fem(k,N,c4n,n4e,Nb,fNodes,r_ball);
+%     M_in = compute_M_in_fem_precise(k,N,c4n,n4e,Nb,fNodes,r_ball);
     M_out = compute_M_outer(k,N,r_ball);
     A = cN*(M_in + M_out)*cN/(2*r_ball);
     dets(i) = det(A);
 end
 
 %% Plot results:
-figure
+figure('Position',[500,600,1000,300])
 contour(real(L),imag(L),log(abs(dets)),100)
 colormap viridis
 colorbar
 hold on
 resonances = islocalmin(abs(dets),1) & islocalmin(abs(dets),2);
-plot(L(resonances),'.','MarkerSize',20,'color','r')
+plot(L(resonances),'.','MarkerSize',10,'color','r')
 hold off
 Resonances = L(resonances)
 drawnow
 %%
 figure
 k_test = -2-0.2i;
-M_in_direct_t = compute_M_in_fem(k_test,N,c4n,n4e,Nb,fNodes,r_ball);
+M_in_direct_t = compute_M_in_fem_precise(k_test,N,c4n,n4e,Nb,fNodes,r_ball);
 M_out_t = compute_M_outer(k_test,N,r_ball);
 A = cN*(M_in_direct_t + M_out_t)*cN/(2*r_ball);
 plot(abs(diag(A-eye(2*N+1))))
 ylim([0,1])
-
 
 
 
