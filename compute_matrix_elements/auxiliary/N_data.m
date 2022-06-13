db7c02fbe7eeeb4a1559c52121d0d682e7395e69
Frank Rosler <frankrosler@FranksWrkLaptop.fritz.box>
2022-06-13 12:16:04 +0200

    cleanup

--- compute_matrix_elements/auxiliary/N_data.m
+++
@@ -1,3 +0,0 @@
-function val = N_data(x)
-    val = exp(3i*atan2(x(2),x(1)));
-end
--- compute_matrix_elements/auxiliary/rhs.m
+++
@@ -1,5 +0,0 @@
-function val = rhs(x)
-% computes right hand side of Poisson equation. Not really needed for our
-% spectral problem!
-    val = 0;
-end
